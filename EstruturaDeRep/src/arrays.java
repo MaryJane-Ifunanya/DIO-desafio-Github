1b3679276c39a2882ddc61c2ee4d1fdfd26f4970
MaryJane Ifunanya <maryjaneifunanya3@gmail.com>
2022-11-15 13:30:55 -0800

    Adicionando exercicios de tabuada e Arrays

---
+++ EstruturaDeRep/src/arrays.java
@@ -0,0 +1,21 @@
+package EstruturaDeRep.src;
+
+import javax.swing.event.SwingPropertyChangeSupport;
+
+public class arrays {
+    public static void main(String[] args){
+        int [] vetor = {-5, -6, 15, 50, 8, 4};
+
+         System.out.println("Vetor: ");
+        int count = 0;
+        while(count < vetor.length){
+            System.out.println(vetor[count]);
+            count++;
+        }
+// de tras pra friente
+
+        for(int i = (vetor.length - 1); i >= 0; i--){
+            System.out.println(vetor[i] + " ");
+        }
+    }
+}
